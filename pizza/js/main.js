3982a8c30689287d510e191f6b286003d17f15f1
frontendDiva <69750159+frontendDiva@users.noreply.github.com>
2020-08-26 11:30:31 +0300

    commitPortfolio

--- pizza/js/main.js
+++ pizza/js/main.js
@@ -2,12 +2,14 @@ $(function () {
     $('.photo__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        /* prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="prev"></button>',
-        nextArrow: ​ '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="next"></button>' */
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="prev"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="next"></button>'
     });
 
-    $('.menu__btn').on('.click', function () {
-        $('.menu__list').slideToggle();
+    var toggleButton = document.querySelector('.menu__btn');
+    var navBar = document.querySelector('.menu');
+    toggleButton.addEventListener('click', function () {
+        navBar.classList.toggle('active-menu');
     });
 
     new WOW().init();
