cf7936998295c36044a9193b4930364fcbd062ce
frontendDiva <69750159+frontendDiva@users.noreply.github.com>
2020-08-29 07:14:26 +0300

    SalonOfBeautyImprove Commit

--- salon-of-beauty/js/main.js
+++ salon-of-beauty/js/main.js
@@ -8,48 +8,4 @@ $(".popup-btn").click(function () {
         $(".hidden-popup").removeClass("active");
         $(".hidden-popup__bg").fadeOut();
     });
-    var form = document.querySelector('.validate-form')
-    var validateBtn = form.querySelector('.submit-btn')
-    var phone = form.querySelector('.form-input')
-    var phoneMatch = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/
-
-
-    form.addEventListener('submit', function () {
-        event.preventDefault()
-          removeValidation()
-          checkFieldsPresence()
-          checkPhoneMatch()
-        console.log('clicked on validate')
-    });
-
-    var removeValidation = function () {
-        var errors = form.querySelectorAll('.error')
-
-        for (var i = 0; i < errors.length; i++) {
-            errors[i].remove()
-        }
-    }
-
-    var checkFieldsPresence = function () {
-        for (var i = 0; i < phone.length; i++) {
-            if (!phone[i].value) {
-                var error = generateError('Пожалуйста, введите телефон')
-                form[i].parentElement.insertBefore(error, fields[i])
-            }
-        }
-    }
-    var checkPhoneMatch = function () {
-        if (phone.value !== phoneMatch) {
-            var error = generateError('Пожалуйста, введите номер телефона')
-            phone.addClass('.error-form')
-            phone.parentElement.insertBefore(error, phone)
-        }
-    }
-    var generateError = function (text) {
-        var error = document.createElement('div')
-        error.className = 'error'
-        error.style.color = 'red'
-        error.innerHTML = text
-        return error
-    }
 });
